c12007ea48cdd97e9b5b9e9ad0c97fb7abb1da64
Guille Cosano <i62cogag@uco.es>
2020-04-02 10:41:45 +0200

    Created hunter entity with api-platform integration

---
+++ src/Entity/Hunter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\HunterRepository")
+ */
+class Hunter
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
