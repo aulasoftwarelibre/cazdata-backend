1df896b166b5f75cd50a1676d59aa84ab502a43c
Sergio Gómez <sergio@uco.es>
2020-04-11 23:05:04 +0200

    Restore unrequired changes

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
