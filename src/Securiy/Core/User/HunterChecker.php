37db03305cdff804e70ed794cf1b3508813eed9b
Sergio Gómez <sergio@uco.es>
2020-04-19 00:13:05 +0200

    Add enabled hunter checker

---
+++ src/Securiy/Core/User/HunterChecker.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Securiy\Core\User;
+
+use App\Entity\Hunter;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class HunterChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user) : void
+    {
+        if (! $user instanceof Hunter) {
+            return;
+        }
+
+        if (! $user->getIsEnabled()) {
+            throw new DisabledException();
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user) : void
+    {
+        if (! $user instanceof Hunter) {
+            return;
+        }
+    }
+}
